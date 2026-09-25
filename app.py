77f6e46aa7faf6110ab5e057656cb2b67c720d87
nizamidialeksandr-wq <nizamidialeksandr@gmail.com>
2026-09-25 14:58:15 +0300

    добавили шаблоны стили скрипты передачу параметров

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, request
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Привет, мир! Flask работает в контейнере."
+    return render_template("index.html")
+
 
 
 @app.route("/hello")
 def hello_name():
-    name = request.args.get("name", "незнакомец")
-    return f"Привет, {name}!"
+    nameparams = request.args.get("name", "незнакомец")
+    return render_template("greating.html", name=nameparams)
+
+@app.route("/1234")
+def one_two_three():
+    a = request.args.get("a", 0)
+    b = request.args.get("b", 0)
+    return str(int(a) + int(b))
+
